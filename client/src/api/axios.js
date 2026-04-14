e0ce1076cb0cffbc43176bb8d0648b82407649f9
dushksendy-eng <dushksendy@gmail.com>
2026-04-14 10:15:54 +0900

    Fix axios baseURL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://solotogether-1.onrender.com/api' })
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+
+const api = axios.create({ baseURL })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('st_token')
